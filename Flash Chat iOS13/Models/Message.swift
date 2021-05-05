e282edf367e644de19382cd70cdeaa73b49020b3
Guilherme Mendes <mendes.silva.guilherme@gmail.com>
2021-05-05 14:27:14 -0300

    Final version

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Guilherme Mendes da Silva on 29/04/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
